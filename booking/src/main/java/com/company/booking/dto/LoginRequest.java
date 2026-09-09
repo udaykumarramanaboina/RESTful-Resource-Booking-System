e3443b28970c9e60347978144ad268f54a149683
udaykumar-tech-admin <udaykumar.ramanaboina@gmail.com>
2026-09-09 12:32:02 +0530

    feat: add signup and login DTOs with required fields

---
+++ booking/src/main/java/com/company/booking/dto/LoginRequest.java
@@ -0,0 +1,6 @@
+package com.company.booking.dto;
+
+public class LoginRequest {
+	private String email;
+	private String password;
+}
---
+++ booking/src/main/java/com/company/booking/dto/SignupRequest.java
@@ -0,0 +1,9 @@
+package com.company.booking.dto;
+
+import lombok.Data;
+
+@Data
+public class SignupRequest {
+	private String email;
+	private String password;
+}
